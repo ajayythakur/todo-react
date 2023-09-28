a3f0c17fdce5efc60ed324e54dc37f8ae97aecb2
ajayythakur <ajay1270panwar@gmail.com>
2023-09-28 20:33:43 +0530

    todo react

--- src/Component/ToDo.js
+++ src/Component/ToDo.js
@@ -1,51 +1,37 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
+import '../App.css'
 
 const ToDo = () => {
-    const [task,setTask] = useState("");
-    const [list,setList] = useState([]);
-    const [completedTask,setCompTask] = useState([]);
-
-    const localStorageKey = "todo:tasks";
-
-    const saveTaskinLS = (newTasks) => {
-        setList(newTasks);
-        console.log(newTasks)
-        localStorage.setItem(localStorageKey, JSON.stringify(newTasks));
-    }
-
-    const getTaskfromLS = () => {
-        const savedTasks = localStorage.getItem(localStorageKey);
-        console.log(savedTasks);
-        if(savedTasks){
-            setTask(JSON.parse(savedTasks));
-        }
-    }
+    const [name, setName] = useState("");
+    const [task, setTask] = useState("");
+    const [list, setList] = useState([]);
+    const [completedTask, setCompTask] = useState([]);
 
     const addTask = () => {
-        setList((list)=>{
-            const newTask = [task,...list];
+        setList((list) => {
+            const newTask = [task, ...list];
             console.log(newTask);
             setTask('');
             return newTask;
         })
 
-  
+
 
     }
 
     const removeTask = (index) => {
-        const updatedList = list.filter((ele,id) => {
-            return index != id;
+        const updatedList = list.filter((ele, id) => {
+            return index !== id;
         })
         setList(updatedList);
     }
 
-    const taskCompleted = (data,index) => {
+    const taskCompleted = (data, index) => {
         removeTask(index);
         setCompTask((completedTask) => {
             const compTaskList = [data, ...completedTask];
-            console.log("completed task",compTaskList);
+            console.log("completed task", compTaskList);
             return compTaskList;
         });
     }
@@ -57,43 +43,56 @@ const ToDo = () => {
     // useEffect(()=>{
     //     getTaskfromLS();
     // },[])
+    useEffect(() => {
+        const name = prompt("Enter your name");
+        setName(name);
+    }, [])
     return (
-   <>
-            <div>
-                <h1>ToDo App</h1>
-                <input type='text' placeholder='Enter Task' value={task} onChange={(e)=>setTask(e.target.value)} />
-                <button className='btn' onClick={addTask} >Add Task</button>
-
-                <h1>Tasks</h1>
+        <>
+            <div className='container'>
+                <h1>Welcome {name}</h1>
+                <input type='text' placeholder='Enter Task' value={task} onChange={(e) => setTask(e.target.value)} /> <br />
+                <button onClick={addTask} >Add Task</button>
                 {list.length >= 1 ? <button onClick={emptyTaskLst}>Remove All Task</button> : ""}
-                {list != [] && list.map((data,index)=>{
-                    return(
+
+                {list.length >= 1 ? <h1>Tasks</h1> : <h1>No Active Tasks</h1>}
+                {list !== [] && list.map((data, index) => {
+                    return (
                         <>
-                        <span key={index}>
-                           <div> {data}
-                           <button onClick={() => taskCompleted(data,index)}>Mark Complete</button>
-                           <button onClick={() => removeTask(index)}>Remove</button>
-                           </div>
-                        </span>
+                            <span key={index}>
+                                <div className='task-list'>
+                                    <div className='task-name'>
+                                        {data}
+                                    </div>
+                                    <div className='task-btn'>
+                                        <span class="material-symbols-outlined" onClick={() => taskCompleted(data, index)}>
+                                            check
+                                        </span>
+                                        <button onClick={() => removeTask(index)}><span class="material-symbols-outlined">
+                                            delete
+                                        </span></button>
+                                    </div>
+                                </div>
+                            </span>
                         </>
                     )
                 })}
-                <h1>Completd Task</h1>
+                {list.length === 0 && completedTask.length > 0 ? <h4>Completed Tasks</h4> : ""}
                 {
-                   completedTask !=[] && completedTask.map((compTask,i)=>{
-                        return(
+                    completedTask !== [] && completedTask.map((compTask, i) => {
+                        return (
                             <>
-                            <span key={i}>
-                                <div> {compTask} </div>
-                            </span>
+                                <span key={i}>
+                                    <div className='completed-task'> {compTask} </div>
+                                </span>
                             </>
                         )
                     })
                 }
-          </div>
-        
-    </>
-  )
+            </div>
+
+        </>
+    )
 }
 
 export default ToDo
